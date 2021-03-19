83e13d2b14c82fa737705993b223194cda8ae78e
Gabriel <gabriel04gh1.gh@gmail.com>
2021-03-19 01:11:02 -0300

    teste

--- js/master.js
+++ js/master.js
@@ -1,3 +1,6 @@
+//Start game
+
+challange = [15, 25, 50]
 const play = document.querySelector('#play')
 const home = document.querySelector('#home')
 
@@ -5,7 +8,18 @@ home.addEventListener('click', () => {
   document.querySelector('.chooseChallenge').style.display = 'none'
   document.querySelector('.homescreen').style.display = 'block'
 })
+
 play.addEventListener('click', () => {
   document.querySelector('.chooseChallenge').style.display = 'block'
   document.querySelector('.homescreen').style.display = 'none'
+
+  let chooseChallenge = document.querySelector('#chooseChallenge')
+  let challanges = ``
+
+  for (var i = 0; i < challange.length; i++) {
+     challanges = `Desafio de ${challange[i]} perguntas.`
+     chooseChallenge.innerHTML += `<p><input type="button" id="C${i}" value="${challanges}"></p>`
+  }
+
+
 })
